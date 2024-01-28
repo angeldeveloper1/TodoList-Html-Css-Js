bfa61bd4b808beb075435a2b123d0143c65e77ad
Angel Chung <angeldeveloper1@gmail.com>
2024-01-28 18:08:01 -0500

    Agregacion de comentarios

--- TodoList/scripts/scripts.js
+++ TodoList/scripts/scripts.js
@@ -1,11 +1,12 @@
+// Array que almacena la lista de tareas obtenida del almacenamiento local
 const todoList = JSON.parse(localStorage.getItem('todoList'))||[];
-
+// Renderiza la lista de tareas en el DOM
 renderTodoList();
-
+// Función para renderizar la lista de tareas 
 function renderTodoList(){
-
+  
   let todoListHTML='';
-
+// Itera sobre la lista de tareas y construye el HTML correspondiente
   for (let i = 0; i < todoList.length; i++){
 
     const todoObject = todoList[i];
@@ -31,13 +32,13 @@ function renderTodoList(){
 
     todoListHTML += html; 
   }
-
+// Inserta el HTML en el contenedor de la lista de tareas
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
 
 
 }
-
+// Función para agregar una nueva tarea a la lista
 function addTodo(){
 
   const inputElement = document.querySelector('.js-name-input');
@@ -45,20 +46,20 @@ function addTodo(){
 
   const dateInputElement = document.querySelector('.js-due-date-input');
   const dueDate = dateInputElement.value;
-
+// Agrega la nueva tarea al array
   todoList.push({
     name,
     dueDate
   });
-
+// Limpia los campos de entrada
   inputElement.value='';
   dateInputElement.value='';
-
+// Renderiza la lista actualizada
   renderTodoList();
-  
+// Guarda la lista en el almacenamiento local
   saveToStorage();
 }
-
+// Función para guardar la lista de tareas en el almacenamiento local
 function saveToStorage(){
   localStorage.setItem('todoList',JSON.stringify(todoList));
 }
