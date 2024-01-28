e27f7fae851898ebd999ad467e39ae9a8df73544
Angel Chung <angeldeveloper1@gmail.com>
2024-01-28 02:58:52 -0500

    Creacion de proyecto TodoList

---
+++ TodoList/scripts/scripts.js
@@ -0,0 +1,64 @@
+const todoList = JSON.parse(localStorage.getItem('todoList'))||[];
+
+renderTodoList();
+
+function renderTodoList(){
+
+  let todoListHTML='';
+
+  for (let i = 0; i < todoList.length; i++){
+
+    const todoObject = todoList[i];
+
+    const name = todoObject.name;
+
+    const dueDate = todoObject.dueDate;
+
+    const html = `
+
+    <div>${name}</div>
+    <div>${dueDate}</div>
+
+    <button 
+    onclick="
+      todoList.splice(${i},1);
+      renderTodoList();
+      saveToStorage();
+    "
+    class="delete-todo-button">
+    Delete
+    </button>`;
+
+    todoListHTML += html; 
+  }
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+
+}
+
+function addTodo(){
+
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value='';
+  dateInputElement.value='';
+
+  renderTodoList();
+  
+  saveToStorage();
+}
+
+function saveToStorage(){
+  localStorage.setItem('todoList',JSON.stringify(todoList));
+}
